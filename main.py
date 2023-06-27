0279f4d766c6b86316eef06968d366fbe4789412
Aveson <avesoncarvalho25@gmail.com>
2023-06-27 13:45:50 +0100

    added return menu function

--- main.py
+++ main.py
@@ -10,25 +10,44 @@ except FileNotFoundError:
     print(CLEAR)
     print("Importion Error : Essential files not found.")
     print("Program shutting down.")
-    sleep(1)
+    sleep(1) #Slows down the program to give better UX
     exit()
 except ModuleNotFoundError:
     print(CLEAR)
     print("Importion Error: Essential modules not found.")
     print("Program shutting down.")
-    sleep(1)
+    sleep(1) #Slows down the program to give better UX
     exit()
 except Exception as e:
     print(CLEAR)
     print(f"An unknown error occurred: {str(e)}")
     print("Program shutting down.")
-    sleep(1)
+    sleep(1) #Slows down the program to give better UX
     exit()
 # -------------------- End of Importing modules -------------
 
 # -------------------- Main code ----------------------------
+# return menu code
+def return_menu():
+    while True:
+        choice = input(f"{spacing}\nDo you want to go back to menu? (Yes or No): \n{spacing}\n")
+        try:
+            choice = str(choice)
+            if choice.lower().strip() in ["yes","ye","yea","y"]:
+                main_menu()
+            elif choice.lower().strip() in ["no","nah","n"]:
+                print("Exiting...")
+                sleep(1) #Slows down the program to give better UX 
+                exit()
+            else:
+                print("Enter either 'Yes' or 'No'.")
+        except ValueError:
+            print("Enter either 'Yes' or 'No'.")
+            continue
+        except Exception:
+            print("There has been an unexpected error. Try again")
 
-# Main Menu code
+# main menu code
 def main_menu():
     print(CLEAR)
     print(f"{spacing}\nMain Menu\n{spacing}")
@@ -40,10 +59,12 @@ def main_menu():
             choice = int(choice)
             if choice == 1:
                 import alarm
-                break
+                sleep(1) #Slows down the program to give better UX 
+                return_menu()
             elif choice == 2:
                 import timer
-                break
+                sleep(1) #Slows down the program to give better UX 
+                return_menu()
             else:
                 print(f"{spacing}\nInvalid choice. Please enter 1 or 2.\n{spacing}")
         except ValueError:
@@ -51,14 +72,8 @@ def main_menu():
         except Exception as e:
             print(f"An unknown error occurred: {str(e)}")
             print("Program shutting down.")
-            sleep(1)
+            sleep(1) #Slows down the program to give better UX
             exit()
-    
-                
-            
-
-
-
 
 main_menu()
 
